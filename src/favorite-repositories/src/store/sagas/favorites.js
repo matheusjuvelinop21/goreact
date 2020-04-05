370756a4252f90ff18c3d1aa746e191f7d93fcf5
matheusjuvelinop21 <matheus.silva@p21sistemas.com.br>
2020-04-05 09:18:50 -0300

    não adicionar reposítorios duplicados

--- src/favorite-repositories/src/store/sagas/favorites.js
+++ src/favorite-repositories/src/store/sagas/favorites.js
@@ -1,9 +1,20 @@
 import api from "../../services/api";
-import { call, put } from "redux-saga/effects";
+import { call, put, select } from "redux-saga/effects";
 import { Creators as FavoriteActions } from "../ducks/favorites";
 
 export default function* addFavorite(action) {
   try {
+    const isDuplicated = yield select((state) =>
+      state.favorites.data.find(
+        (favorite) => favorite.name === action.payload.repository
+      )
+    );
+
+    if (isDuplicated)
+      return yield put(
+        FavoriteActions.addFavoriteFailure("Repositório duplicado")
+      );
+
     const { data } = yield call(api.get, `repos/${action.payload.repository}`);
 
     yield put(
@@ -15,6 +26,7 @@ export default function* addFavorite(action) {
       })
     );
   } catch (error) {
+    console.error(error);
     yield put(
       FavoriteActions.addFavoriteFailure("Error ao adicionar repositório")
     );
